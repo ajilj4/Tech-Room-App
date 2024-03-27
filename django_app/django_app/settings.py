1201b6bbc87fcee0d49d3955ff09aaffba0cd0c6
ajilj4 <ajilj4@gmail.com>
2024-03-28 01:12:32 +0530

    now in settings allowed host,debug,secretkey changed for hosting

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ztnlek98gp#bb0z)$j52$!g=+o2f)1ir8328!4mak=6+itxm28'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG','False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','').split(',')
 
 
 # Application definition
@@ -47,6 +47,7 @@ AUTH_USER_MODEL = 'baseapp.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
